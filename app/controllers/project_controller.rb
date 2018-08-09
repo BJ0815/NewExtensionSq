09e5012b548f40be14798be4808ff9282cd538f2
Tripresso <tripresso@Tripressode-MacBook-Pro-2.local>
2018-08-09 18:07:44 +0800

    [Chg]新增各頁面資料

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,9 +1,13 @@
 class ProjectController < ApplicationController
+  # after_action :render_page
+
   def index
   end
 
+  # 新支線
   def productV1
-     # @user = flickr.people.findByUsername(:username => 'yaoxiangz')
+    @title = '新支線'
+    # @user = flickr.people.findByUsername(:username => 'yaoxiangz')
     # 所有照片
     @photos = flickr.people.getPublicPhotos :user_id => "102003626@N07"
     
@@ -12,7 +16,194 @@ class ProjectController < ApplicationController
     
     # 相簿照片
     # type [ id,primary,owner,owername,photo[{}] ]
-    @album_1 = flickr.photosets.getPhotos :photoset_id => "72157671596816948",:extras => "url_m"
-
+    @album_1 = flickr.photosets.getPhotos :photoset_id => "72157671596816948",:extras => "url_s"
   end
+
+  def productV2
+    id = "72157691374202464"
+
+    @album_1 = flickr.photosets.getPhotos :photoset_id => id,:extras => "url_s"
+
+    @info = flickr.photosets.getInfo :photoset_id => id
+    @title = @info.title
+    render :productV1
+  end
+
+  def productV3
+    id = "72157664586091678"
+
+    @album_1 = flickr.photosets.getPhotos :photoset_id => id,:extras => "url_s"
+
+    @info = flickr.photosets.getInfo :photoset_id => id
+    @title = @info.title
+    render :productV1
+  end
+
+  def productV4
+    id = "72157689314292912"
+
+    @album_1 = flickr.photosets.getPhotos :photoset_id => id,:extras => "url_s"
+
+    @info = flickr.photosets.getInfo :photoset_id => id
+    @title = @info.title
+    render :productV1
+  end
+
+  def productV5
+    id = "72157691374978194"
+
+    @album_1 = flickr.photosets.getPhotos :photoset_id => id,:extras => "url_s"
+
+    @info = flickr.photosets.getInfo :photoset_id => id
+    @title = @info.title
+    render :productV1
+  end
+
+  def productV6
+    id = "72157664587379988"
+
+    @album_1 = flickr.photosets.getPhotos :photoset_id => id,:extras => "url_s"
+
+    @info = flickr.photosets.getInfo :photoset_id => id
+    @title = @info.title
+    render :productV1
+  end
+
+  def productV7
+    id = "72157698928904994"
+
+    @album_1 = flickr.photosets.getPhotos :photoset_id => id,:extras => "url_s"
+
+    @info = flickr.photosets.getInfo :photoset_id => id
+    @title = @info.title
+    render :productV1
+  end
+
+  def productV8
+    id = "72157697706718151"
+
+    @album_1 = flickr.photosets.getPhotos :photoset_id => id,:extras => "url_s"
+
+    @info = flickr.photosets.getInfo :photoset_id => id
+    @title = @info.title
+    render :productV1
+  end
+
+  def productV8
+    id = "72157699356527965"
+
+    @album_1 = flickr.photosets.getPhotos :photoset_id => id,:extras => "url_s"
+
+    @info = flickr.photosets.getInfo :photoset_id => id
+    @title = @info.title
+    render :productV1
+  end
+
+  # ===================================
+
+  def weddingV1
+    id = "72157671471188328"
+
+    @album_1 = flickr.photosets.getPhotos :photoset_id => id,:extras => "url_s"
+
+    @info = flickr.photosets.getInfo :photoset_id => id
+    @title = @info.title
+    render :productV1
+  end
+
+  def weddingV2
+    id = "72157693547049570"
+
+    @album_1 = flickr.photosets.getPhotos :photoset_id => id,:extras => "url_s"
+
+    @info = flickr.photosets.getInfo :photoset_id => id
+    @title = @info.title
+    render :productV1
+  end
+
+  def weddingV3
+    id = "72157699367504775"
+
+    @album_1 = flickr.photosets.getPhotos :photoset_id => id,:extras => "url_s"
+
+    @info = flickr.photosets.getInfo :photoset_id => id
+    @title = @info.title
+    render :productV1
+  end
+
+  # ===================================
+
+  def businessV1
+    id = "72157675671761441"
+
+    @album_1 = flickr.photosets.getPhotos :photoset_id => id,:extras => "url_s"
+
+    @info = flickr.photosets.getInfo :photoset_id => id
+    @title = @info.title
+    render :productV1
+  end
+
+  def businessV2
+    id = "72157671481556448"
+
+    @album_1 = flickr.photosets.getPhotos :photoset_id => id,:extras => "url_s"
+
+    @info = flickr.photosets.getInfo :photoset_id => id
+    @title = @info.title
+    render :productV1
+  end
+
+  def businessV3
+    id = "72157677178014326"
+
+    @album_1 = flickr.photosets.getPhotos :photoset_id => id,:extras => "url_s"
+
+    @info = flickr.photosets.getInfo :photoset_id => id
+    @title = @info.title
+    render :productV1
+  end
+
+  def businessV3
+    id = "72157699356903405"
+
+    @album_1 = flickr.photosets.getPhotos :photoset_id => id,:extras => "url_s"
+
+    @info = flickr.photosets.getInfo :photoset_id => id
+    @title = @info.title
+    render :productV1
+  end
+
+  # ===================================
+
+  def liveV1
+    id = "72157673573325473"
+
+    @album_1 = flickr.photosets.getPhotos :photoset_id => id,:extras => "url_s"
+
+    @info = flickr.photosets.getInfo :photoset_id => id
+    @title = @info.title
+    render :productV1
+  end
+
+  def liveV1
+    id = "72157696176285902"
+
+    @album_1 = flickr.photosets.getPhotos :photoset_id => id,:extras => "url_s"
+
+    @info = flickr.photosets.getInfo :photoset_id => id
+    @title = @info.title
+    render :productV1
+  end
+
+
+
+  # def render_page
+  #   id = @id
+
+  #   @album_1 = flickr.photosets.getPhotos :photoset_id => id,:extras => "url_s"
+
+  #   @info = flickr.photosets.getInfo :photoset_id => id
+  #   @title = @info.title
+  #   render :productV1
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,31 @@
 Rails.application.routes.draw do
   get 'project/index'
 
+  # 個人創作
   get 'project/productV1'
+  get 'project/productV2'
+  get 'project/productV3'
+  get 'project/productV4'
+  get 'project/productV5'
+  get 'project/productV6'
+  get 'project/productV7'
+  get 'project/productV8'
+
+
+  # 商業紀實
+  get 'project/weddingV1'
+  get 'project/weddingV2'
+  get 'project/weddingV3'
+  get 'project/businessV1'
+  get 'project/businessV2'
+  get 'project/businessV3'
+  get 'project/businessV4'
+
+
+  # 演唱會紀實
+  get 'project/liveV1'
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
